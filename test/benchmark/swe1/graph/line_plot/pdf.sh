d8f8a86f836a15953532742c0b8974112ca2cf84
the96 <aar30px2@gmail.com>
2019-12-11 18:54:17 +0900

    cp graph -> swe1

---
+++ test/benchmark/swe1/graph/line_plot/pdf.sh
@@ -0,0 +1,32 @@
+TEX=graph
+
+function maketex() {
+    cat <<EOF
+\documentclass[a4j]{jarticle}
+\usepackage{graphicx}
+\newcommand{\graph}[1]{\includegraphics[bb=0 0 864 576, width=.5\hsize]{#1}}
+\begin{document}
+EOF
+
+    odd=0
+    for i in *.png
+    do
+	if [ $odd = 1 ]
+	then
+	    echo '\graph{'$i'}\\\\'
+	else
+	    echo '\graph{'$i'}'
+	fi
+	odd=$(((odd+1)%2))
+    done
+
+cat <<EOF
+\end{document}
+EOF
+}
+
+maketex > $TEX.tex
+platex $TEX.tex
+dvipdfmx $TEX.dvi
+
+rm graph.aux graph.dvi graph.log graph.tex
